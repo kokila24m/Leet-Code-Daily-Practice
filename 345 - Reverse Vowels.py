2b5820caaea9591eaa48ec03e0ca2a6a55023aaf
“kokila24m” <kokila24m@gmail.com>
2025-04-27 11:49:45 +0530

    Adding code of day 5

---
+++ 345 - Reverse Vowels.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    """
+    Logic :
+    
+    Two pointers approach.
+    Swap vowels from both ends.
+    """
+
+    def reverseVowels(self, s):
+        vowels = set("aeiouAEIOU")
+        s = list(s)
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            while left < right and s[left] not in vowels:
+                left += 1
+            while left < right and s[right] not in vowels:
+                right -= 1
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        
+        return "".join(s)
